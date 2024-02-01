3f79e43696763225d0a6db325cf33f78069e9afa
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-01 11:19:48 +0100

    feat: test component duck

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import Comp from './comp.svelte';
+	import { Value } from '@sinclair/typebox/value';
+
 	export let data;
 	let { organizations } = data;
 </script>
 
 {#each organizations as organization}
+	<Comp data={organization}></Comp>
 	<pre>{JSON.stringify(organization, null, 2)}</pre>
 {/each}
---
+++ src/routes/comp.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Type, type Static } from '@sinclair/typebox';
+
+	const c = Type.Object({
+		name: Type.String()
+	});
+
+	export let data: Static<typeof c>;
+</script>
+
+<pre>{data.name}</pre>
+
+<style>
+	pre {
+		font-size: 40px;
+	}
+</style>
