250e507191c4e23d4ebd2a7e575825bd353d017c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-07 08:18:10 +0100

    feat: update landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 </script>
 
-<h1>Work in prooooooog</h1>
+<div class="flex h-screen w-screen flex-col items-center justify-center gap-6 bg-stone-200">
+	<p>Sito web di Giovanni Abbatepaolo</p>
+	<h1 class="-rotate-3 rounded-xl border border-stone-700 bg-white p-6 text-5xl">
+		Work in progress! 🏗️👷‍♀️
+	</h1>
+	<div class="flex gap-2">
+		<a href="https://www.instagram.com/bbtgnn/" target="_blank">instagram</a>
+		<a href="mailto:giovanniabb@gmail.com" target="_blank">email</a>
+	</div>
+</div>
+
 <!-- {#each organizations as organization}
 	<Comp data={organization}></Comp>
 	<pre>{JSON.stringify(organization, null, 2)}</pre>
 {/each} -->
+
+<style lang="postcss">
+	a {
+		@apply underline hover:no-underline;
+	}
+</style>
