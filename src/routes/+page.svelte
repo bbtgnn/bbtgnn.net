0ad4fe49b3ebbe2e86923120e55c9041fe5e4676
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-08 15:50:46 +0100

    feat: homepage update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,46 @@
 <script lang="ts">
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center gap-6 bg-stone-200">
-	<p>Sito web di Giovanni Abbatepaolo</p>
-	<h1 class="-rotate-3 rounded-xl border border-stone-700 bg-white p-6 text-5xl">
-		Work in progress! 🏗️👷‍♀️
-	</h1>
-	<div class="flex gap-2">
-		<a href="https://www.instagram.com/bbtgnn/" target="_blank">instagram</a>
-		<a href="mailto:giovanniabb@gmail.com" target="_blank">email</a>
+<div
+	class="flex h-screen w-screen flex-col items-center justify-center gap-7 bg-stone-200 p-6 font-mono"
+>
+	<div class="flex flex-col items-center">
+		<p class="font-bold">Hello! 👋</p>
+		<p>I'm Giovanni Abbatepaolo 🌲</p>
+	</div>
+	<div class="flex flex-col items-center">
+		<p class="font-bold">I do</p>
+		<div class="flex flex-wrap justify-center gap-x-3">
+			<p class="text-center">👨‍💻 Web development</p>
+			<p class="text-center">🏗️ Information architecture</p>
+			<p class="text-center">📚 Teaching</p>
+			<p class="text-center">🪢 UI/UX</p>
+			<p class="text-center">🎨 Graphic design</p>
+		</div>
+	</div>
+	<div class="flex flex-col items-center">
+		<p>My website is...</p>
+		<h1 class="-rotate-3 rounded-xl border border-stone-700 bg-white p-6 text-3xl">
+			Under construction! 🏗️👷‍♀️
+		</h1>
+	</div>
+	<div class="flex flex-col items-center">
+		<p class="font-bold">ℹ️ More</p>
+		<div class="flex flex-wrap justify-center gap-x-3">
+			<a class="text-center" href="https://www.instagram.com/bbtgnn/" target="_blank">
+				📷 instagram
+			</a>
+			<a
+				class="text-center"
+				href="https://giovanniabbatepaolo.notion.site/458ab8fdff2742bd817fdd6ac820e188?v=cda461a173de48e9b30d487b7f741fd2&pvs=4"
+				target="_blank"
+			>
+				📘 portfolio (creative coding)
+			</a>
+			<a class="text-center" href="mailto:giovanniabb@gmail.com" target="_blank">📫 email</a>
+		</div>
 	</div>
 </div>
-
-<!-- {#each organizations as organization}
-	<Comp data={organization}></Comp>
-	<pre>{JSON.stringify(organization, null, 2)}</pre>
-{/each} -->
 
 <style lang="postcss">
 	a {
