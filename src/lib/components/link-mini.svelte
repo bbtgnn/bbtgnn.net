0e2066e746fb24d58a135a3197fa210d3c8413a5
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-07-29 11:41:20 +0200

    setup site

---
+++ src/lib/components/link-mini.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { ArrowUpRight } from '@lucide/svelte';
+
+	type Props = {
+		href: string;
+	};
+
+	let { href }: Props = $props();
+</script>
+
+<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+<a {href} class="inline-flex">
+	<ArrowUpRight size={12} />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import LinkMini from '$lib/components/link-mini.svelte';
+</script>
+
+<main>
+	<div class="group">
+		<p>Ciao! 👋</p>
+		<p>Sono Giovanni Abbatepaolo 🌲</p>
+	</div>
+
+	<div class="group">
+		<p>Mi occupo di</p>
+		<ul class="skills">
+			<li class="underline decoration-didattica">📚 Didattica</li>
+			<li class="underline decoration-architettura">🏗️ Architettura dell'informazione</li>
+			<li class="underline decoration-sviluppo">👨‍💻 Sviluppo web</li>
+			<li class="underline decoration-ui-ux-design">🪢 UI/UX design</li>
+			<li class="underline decoration-graphic-design">🎨 Graphic design</li>
+		</ul>
+	</div>
+
+	<div class="group">
+		<p>Dove lavoro</p>
+		<ul>
+			<li>
+				<p>
+					Accademia di Belle Arti di Perugia
+					<LinkMini href="https://www.abaperugia.com/author/abbatepaologiovanni/" />
+				</p>
+			</li>
+		</ul>
+	</div>
+</main>
+
+<style lang="postcss">
+	@reference "./layout.css";
+
+	main {
+		@apply space-y-4 font-mono;
+	}
+
+	.group p:first-child {
+		@apply font-bold;
+	}
+
+	.underline {
+		@apply underline-offset-2;
+		text-decoration-thickness: 0.25rem;
+	}
+</style>
